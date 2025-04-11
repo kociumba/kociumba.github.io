7d81724ac39dd0a1b92dcb8301d0fcfec837828b
kociumba <79542688+kociumba@users.noreply.github.com>
2025-04-11 20:10:33 +0200

    more funny ads

--- annoying.js
+++ annoying.js
@@ -21,11 +21,27 @@ function activateAnnoyingMode() {
     carousel.className = "banner-carousel";
 
     const adTexts = [
-        "🚨 Call +00-420-69-2137 to get 50% off your next google search 🚨",
+        "🚨 Call +0-420-69-2137 to get 50% off your next google search",
         "Rumors are Tariffs have been enforced on package managers 🤔, click here to see if your package manager is affected !!!",
         "Subscribe to WasteYourMoney Ltd just for 19.99$/second",
-        "Experts say: \"In the next 50 years sunliight might become the next big thing next to crypto and ai\"",
-        "Buy 1 nothing now and get 2 more compleatly free 🤯"
+        "Experts say: \"In the next 50 years sunlight might become the next big thing next to crypto and ai\"",
+        "Buy 1 nothing now and get 2 more compleatly free 🤯",
+        "BREAKING: vibe coder ships product with only 100 bugs 🐛",
+        "bilons must go",
+        "are you paying attention ?",
+        "Cat distribution system under heavy load after mistakenly delivering a dog 🐈",
+        "\"To your right you will notice a wild tech bro in it's natural habitat\"",
+        "Interview with the local rust dev: \"Rust really changed my life, I always wanted a girlfriend and now I am a girlfirend\"",
+        "still reading ?",
+        "BREAKING: something happend 🤯",
+        "python devs in shock, as they try a language with a type system",
+        "Nvidia announces the \"It's worse than last gen, but you will still buy it\" series of graphics cards 🤷‍♂️",
+        "A suspicous big gathering of plants is still under investigation, insiders say all signs point to it being a so called \"forrest\" 🌳",
+        "you know this is a protfolio not a news website ?",
+        "BREAKING: china says catgirl technology will be developed by 2034 📝",
+        "Ever tried buying boredom? We've got a special deal just for you.",
+        "Depression premium is now availible in stores, now with 25% more sad 🥲",
+        "[This ad was removed by a moderator]"
     ];
 
     adTexts.forEach(text => {
